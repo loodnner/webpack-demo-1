6d2d12732ff64ed323d9dcb464fd3288c50a62d9
loodnn <loudjane@163.com>
2021-03-31 22:42:33 +0800

    cssloader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"); //installed via npm
 const path = require("path");
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({ title: "halo", template: "src/assets/index.html" }),
+  ],
 };
